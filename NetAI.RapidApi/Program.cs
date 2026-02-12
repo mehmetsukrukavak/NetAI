42aad4526ebf1d8fafdf678d7de8b453b3de522a
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2026-02-12 13:42:29 +0300

    RapidAPI

--- NetAI.RapidApi/Program.cs
+++ NetAI.RapidApi/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Http.Headers;
+using NetAI.RapidApi.ViewModels;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+List<ApiSeriesViewModel>? list = new List<ApiSeriesViewModel>();
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "08c5c64495msh61c97ae9bfc26f4p1fd526jsn7ec756c6d29c" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    list = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    list?.ForEach(x => 
+        Console.WriteLine(x.rank + " " + x.title + " Film Puanı : " + x.rating + " Yapım Yılı : " + x.year));
+}
+
+request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city?city=%C4%B0stanbul&lang=TR"),
+    Headers =
+    {
+        { "x-rapidapi-key", "08c5c64495msh61c97ae9bfc26f4p1fd526jsn7ec756c6d29c" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
---
+++ NetAI.RapidApi/ViewModels/ApiSeriesViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetAI.RapidApi.ViewModels;
+
+public class ApiSeriesViewModel
+{
+    public int rank { get; set; }
+    public string title { get; set; }
+    public double rating { get; set; }
+    public string year { get; set; }
+}
